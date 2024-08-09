293b60fc991bbdb7224e00229d14ced58f32e20c
ashlynmaria <ashlynbenoy2004@gmail.com>
2024-08-09 00:31:19 -0600

    Added session management to all quizzes, and also added firebase functionality to keep track of the quizzes the users have taken.

--- src/Components/B_Quiz.jsx
+++ src/Components/B_Quiz.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const MissionPlanetHopper2 = (props) => {
   function handleQuizPage(){
-    props.setQuizPage(5);
+    props.setQuizPage(4);
     //4 for MCQ
     //5 for DragNDrop
     //6 for Matching
